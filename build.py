85f38147d2d32b5ba1ddb2905aef48d016b55992
Isaac Castillo <ircastil@ucsd.edu>
2020-01-18 13:44:33 -0800

    added another commit

---
+++ build.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+import subprocess
+import os
+import shutil
+import sys
+
+
+curr_folder = os.path.basename(os.getcwd())
+if curr_folder != 'rpg2':
+    print("please run this script from rpg2 directory")
+    print("current directory: ", (curr_folder))
+    sys.exit()
+
+#make a build directory for CMake
+if os.path.exists('build'):
+    shutil.rmtree('build')
+os.mkdir('build')
+os.chdir('build')
+res = subprocess.run(['cmake', '..'], check=True)
+res = subprocess.run(['cmake', '--build', '.'], check=False)
+
+if res.returncode == 0:
+    shutil.move('src/rpg2', '..')
---
+++ build/src/CMakeFiles/rpg2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpg2.dir/main.cpp.o"
+  "rpg2.pdb"
+  "rpg2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpg2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
